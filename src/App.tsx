d8e78f6989e91957e83237dec77b801df681d2e0
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2025-05-16 13:52:02 -0300

    upadate: description hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-
-import './App.css'
-import Header from './components/Header/Header'
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-
   return (
     <>
       <Header />
+      <main className="">
+        <section className="w-full h-[100dvh] flex items-center">
+          <div className="container mx-auto">
+            <h1 className="text-6xl text-white text-center">
+              Olá, sou <span className="text-amber-500 font-bold">Andersen Cypriano</span>
+            </h1>
+            <p className="mt-4 text-center text-white max-w-[50rem] mx-auto">
+              Sou desenvolvedor front-end com experiência na criação de interfaces modernas e responsivas, utilizando HTML, CSS, JavaScript, TypeScript, React e Next.js. Trabalho com integração de APIs e foco em performance, usabilidade e boas práticas de desenvolvimento. Estou sempre em busca de evolução contínua e soluções criativas para cada projeto.
+            </p>
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,10 +1,31 @@
-import { LuGithub } from "react-icons/lu";
+import { LuFileDown, LuGithub, LuLinkedin } from "react-icons/lu";
 
 export default function SocialLinks() {
   return (
     <div className="flex gap-4">
-      <a href="https://github.com/andrecypriano" target="_blank" rel="noopener noreferrer">
-        <LuGithub size={22} />
+      <a
+        href="https://github.com/andersencypriano"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="rounded border-1 border-b-bg p-1"
+      >
+        <LuGithub size={22} strokeWidth={1.5} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/andrecypriano/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="rounded border-1 border-b-bg p-1"
+      >
+        <LuLinkedin size={22} strokeWidth={1.5} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/andrecypriano/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="rounded border-1 border-b-bg p-1"
+      >
+        <LuFileDown size={22} strokeWidth={1.5} />
       </a>
     </div>
   );
