a1610e350730faca54bca85108e9d64ef0a5bec9
Andersen Cypriano <andersen.cypriano@gmail.com>
2025-05-23 18:06:52 -0300

    fix: change function to navigation

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,14 +1,20 @@
-'use client'
+"use client";
 
-export default function MainMenu () {
-  function handleClick (event: React.MouseEvent<HTMLAnchorElement>) {
-    event.preventDefault()
-    const targetId = event.currentTarget.getAttribute('href')?.substring(1);
+export default function MainMenu() {
+  function handleClick(event: React.MouseEvent<HTMLAnchorElement>) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href")?.substring(1);
 
     if (targetId) {
-      const targetElement = document.getElementById(targetId)
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {;
+        window.scrollTo({
+          behavior: "smooth",
+          top:
+            targetElement.getBoundingClientRect().top -
+            document.body.getBoundingClientRect().top -
+            100,
+        });
       }
     }
   }
@@ -16,15 +22,21 @@ export default function MainMenu () {
     <div className="w-1/2 flex flex-col items-center justify-center">
       <ul className="flex gap-2.5">
         <li className="flex items-center justify-center">
-          <a href="#experiencia" onClick={handleClick} className="text-bg">Experiência</a>
+          <a href="#experiencia" onClick={handleClick} className="text-bg">
+            Experiência
+          </a>
         </li>
         <li className="flex items-center justify-center">
-          <a href="#projetos" onClick={handleClick} className="text-bg">Projetos</a>
+          <a href="#projetos" onClick={handleClick} className="text-bg">
+            Projetos
+          </a>
         </li>
         <li className="flex items-center justify-center">
-          <a href="#tecnologias" onClick={handleClick} className="text-bg">Tecnologias</a>
+          <a href="#tecnologias" onClick={handleClick} className="text-bg">
+            Tecnologias
+          </a>
         </li>
       </ul>
     </div>
-  )
+  );
 }
