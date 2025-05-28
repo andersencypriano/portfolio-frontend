ad6b1ed5ee01c41725655ef43988f70dc51833d5
Andersen Cypriano <andersen.cypriano@gmail.com>
2025-05-28 18:02:59 -0300

    feat: button download

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,14 @@ function App() {
       <Header />
       <main className="">
         <section className="w-full h-[70dvh] flex items-center mb-4 bg-bg" id="sobre">
-          <div className="container mx-auto">
+          <div className="container mx-auto mt-24">
             <h1 className="text-6xl text-white text-center">
               Olá, sou <span className="text-amber-500 font-bold">Andersen Cypriano</span>
             </h1>
-            <p className="mt-4 text-center text-white max-w-[50rem] mx-auto">
+            <p className="my-4 text-center text-white max-w-[50rem] mx-auto">
               Sou desenvolvedor front-end com experiência na criação de interfaces modernas e responsivas, utilizando HTML, CSS, JavaScript, TypeScript, React e Next.js. Trabalho com integração de APIs e foco em performance, usabilidade e boas práticas de desenvolvimento. Estou sempre em busca de evolução contínua e soluções criativas para cada projeto.
             </p>
+            <a href="#" className="w-full lg:w-auto"><button data-aos="fade-up" data-aos-duration="800" className="w-full mx-auto lg:w-auto sm:px-6 py-2 sm:py-3 rounded-lg bg-gradient-to-r from-[#6366f1] to-[#a855f7] text-white font-medium transition-all duration-300 hover:scale-105 flex items-center justify-center lg:justify-start gap-2 shadow-lg hover:shadow-xl animate-bounce-slow aos-init aos-animate"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-file-text w-4 h-4 sm:w-5 sm:h-5"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path><path d="M14 2v4a2 2 0 0 0 2 2h4"></path><path d="M10 9H8"></path><path d="M16 13H8"></path><path d="M16 17H8"></path></svg> Download CV</button></a>
           </div>
         </section>
         <section className="p-5 bg-white" id="experiencia">
